b1136ac95940ee1fcf7728fb16e7d55f34d6bfac
Dylan <s89673827@gmail.com>
2021-04-08 10:55:56 +0800

    Initial Commit

---
+++ KK-hw3/KK_hw3App.swift
@@ -0,0 +1,17 @@
+//
+//  KK_hw3App.swift
+//  KK-hw3
+//
+//  Created by 朱冠儒 on 2021/4/8.
+//
+
+import SwiftUI
+
+@main
+struct KK_hw3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
